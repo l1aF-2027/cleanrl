f7a0bc1281ff3e8237f624552c857746ee8aa2fb
l1aF-2027 <22520460@gm.uit.edu.vn>
2025-01-08 23:22:26 +0700

    change render type

--- cleanrl/ddpg_continuous_action.py
+++ cleanrl/ddpg_continuous_action.py
@@ -68,7 +68,7 @@ class Args:
 def make_env(env_id, seed, idx, capture_video, run_name):
     def thunk():
         if capture_video and idx == 0:
-            env = gym.make(env_id, render_mode="rgb_array")
+            env = gym.make(env_id, render_mode="human")
             env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
         else:
             env = gym.make(env_id)

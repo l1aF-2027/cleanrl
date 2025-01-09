935c5ad827d7fb8432d7af8aae9017b094d2227d
l1aF-2027 <22520460@gm.uit.edu.vn>
2025-01-09 22:07:59 +0700

    change render func

--- cleanrl/td3_continuous_action.py
+++ cleanrl/td3_continuous_action.py
@@ -69,13 +69,17 @@ class Args:
 
 def make_env(env_id, seed, idx, capture_video, run_name):
     def thunk():
-        if capture_video and idx == 0:
-            env = gym.make(env_id, render_mode="rgb_array")
-            env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
-        else:
-            env = gym.make(env_id)
+        env = gym.make(env_id, render_mode="rgb_array")
         env = gym.wrappers.RecordEpisodeStatistics(env)
         env.action_space.seed(seed)
+        
+        if capture_video:
+            env = gym.wrappers.RecordVideo(
+                env, 
+                f"videos/{run_name}", 
+                episode_trigger=lambda episode_id: episode_id > 0 and episode_id % 1999 == 0
+            )
+        
         return env
 
     return thunk

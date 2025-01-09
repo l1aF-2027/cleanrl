1893b838b7e61b2d66229d59ec506c815e8be4b3
l1aF-2027 <22520460@gm.uit.edu.vn>
2025-01-09 09:41:49 +0700

    change record func

--- cleanrl/ddpg_continuous_action.py
+++ cleanrl/ddpg_continuous_action.py
@@ -67,7 +67,7 @@ class Args:
 
 def make_env(env_id, seed, idx, capture_video, run_name):
     def thunk():
-        env = gym.make(env_id)
+        env = gym.make(env_id, render_mode="rgb_array")
         env = gym.wrappers.RecordEpisodeStatistics(env)
         env.action_space.seed(seed)
         

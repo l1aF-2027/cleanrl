fd27cb0c50f11db41d2b310e40549a24cb256e1c
l1aF-2027 <22520460@gm.uit.edu.vn>
2025-01-09 10:22:38 +0700

    del print sps

--- cleanrl/ddpg_continuous_action.py
+++ cleanrl/ddpg_continuous_action.py
@@ -244,7 +244,6 @@ poetry run pip install "stable_baselines3==2.0.0a1"
                 writer.add_scalar("losses/qf1_values", qf1_a_values.mean().item(), global_step)
                 writer.add_scalar("losses/qf1_loss", qf1_loss.item(), global_step)
                 writer.add_scalar("losses/actor_loss", actor_loss.item(), global_step)
-                print("SPS:", int(global_step / (time.time() - start_time)))
                 writer.add_scalar("charts/SPS", int(global_step / (time.time() - start_time)), global_step)
 
     if args.save_model:
